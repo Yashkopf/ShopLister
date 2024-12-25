f89d7753f870fd9a92b151c6fe02bbd71e5855c5
Yashkopf <133718219+Yashkopf@users.noreply.github.com>
2024-12-25 17:20:58 +0200

    Change view model

--- app/src/main/java/com/example/shoplister/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoplister/presentation/MainActivity.kt
@@ -26,7 +26,6 @@ class MainActivity : AppCompatActivity() {
         viewModel.shopList.observe(this) {
             Log.d("MainActivityTest",it.toString())
         }
-
         viewModel.getShopList()
     }
 }
